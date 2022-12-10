075ec9a7fdcab7b1711bcff7e7dc7d0e7429ff0a
Jeremy Cleland <jeremy@clelandco.com>
2022-12-09 22:58:27 -0500

    Updated JS

--- js/app.js
+++ js/app.js
@@ -9,60 +9,78 @@ let tableSelector = document.getElementById('stores');
 
 function header() {
 
-  let trElem = document.createElement('tr');
-  tableSelector.appendChild(trElem);
+  let row1 = document.createElement('tr');
+  tableSelector.appendChild(row1);
 
   let thElem = document.createElement('th');
   thElem.textContent = 'Stores';
-  trElem.appendChild(thElem);
+  row1.appendChild(thElem);
 
   for (let j = 0; j < hours.length; j++) {
     thElem = document.createElement('th');
     thElem.textContent = hours[j];
-    trElem.appendChild(thElem);
+    row1.appendChild(thElem);
   }
   thElem = document.createElement('th');
   thElem.textContent = 'Total';
-  trElem.appendChild(thElem);
+  row1.appendChild(thElem);
 }
 
+// Call header prior to render method
 header();
 
 Store.prototype.render = function () {
 
-  let trElem = document.createElement('tr');
-  tableSelector.appendChild(trElem);
+  let row2 = document.createElement('tr');
+  tableSelector.appendChild(row2);
 
   let tdElem = document.createElement('td');
   tdElem.textContent = this.name;
-  trElem.appendChild(tdElem);
+  row2.appendChild(tdElem);
 
   for (let i = 0; i < hours.length; i++) {
     let tableData = document.createElement('td');
     tableData.textContent = this.cookiesSold[i];
 
-    trElem.appendChild(tableData);
+    row2.appendChild(tableData);
 
   }
   let totalTableData = document.createElement('td');
   totalTableData.textContent = this.total;
-  trElem.appendChild(totalTableData);
+  row2.appendChild(totalTableData);
 };
 
+
+//************** FOOTER **************/
+function footer() {
+  let row3 = document.createElement('tr');
+  tableSelector.appendChild(row3);
+
+  let thElem = document.createElement('th');
+  thElem.textContent = 'Total';
+  row3.appendChild(thElem);
+
+  // for (let i = 0; i < hours.length; i++) {
+  //   for (let j = 0; j < cookiesSold.length; j++) {
+  //     thElem = document.createElement('th');
+  //     total =+ cookiesSold[i] + hours[i];
+  //   }
+  // }
+}
+
+
 function grandTotals() {
-  let grandTotals = document.createElement('tr');
+  let grandTotals = document.createElement('th');
   tableSelector.appendChild(grandTotals);
 
 }
 
-// *************** New Table event ************************
+// *************** Form to add new Store Locations ************************
 
 let myForm = document.getElementById('form');
 
-
-// *********************** HELPER FUNCTIONS UTILITIES   ***************************************
-
 // ************************* CONSTRUCTOR **********************
+
 function Store(name, minCust, maxCust, averageCookie) {
   this.name = name;
   this.minCust = minCust;
@@ -73,11 +91,11 @@ function Store(name, minCust, maxCust, averageCookie) {
 
 }
 
-let Seattle = new Store('Seattle', 23, 65, 6.3, [], 0);
-let Tokyo = new Store('Tokyo', 3, 24, 1.2, [], 0);
-let Dubai = new Store('Dubai', 11, 38, 3.7, [], 0);
-let Paris = new Store('Paris', 20, 38, 2.3, [], 0);
-let Lima = new Store('Lima', 2, 16, 4.6, [], 0);
+let Seattle = new Store('Seattle', 23, 65, 6.3);
+let Tokyo = new Store('Tokyo', 3, 24, 1.2);
+let Dubai = new Store('Dubai', 11, 38, 3.7);
+let Paris = new Store('Paris', 20, 38, 2.3);
+let Lima = new Store('Lima', 2, 16, 4.6);
 
 let cities = [Seattle, Tokyo, Dubai, Paris, Lima];
 
@@ -95,23 +113,27 @@ Store.prototype.cookieSales = function () {
   }
 };
 
+
+// Calling cookieSales Method to get Table Data
 Seattle.cookieSales();
 Tokyo.cookieSales();
 Dubai.cookieSales();
 Paris.cookieSales();
 Lima.cookieSales();
 
+
+// Calling render Method to render Table Data
 Seattle.render();
 Tokyo.render();
 Dubai.render();
 Paris.render();
 Lima.render();
-
-console.log(Seattle);
-console.log(Tokyo);
-console.log(Dubai);
-console.log(Paris);
-console.log(Lima);
+footer();
+// console.log(Seattle);
+// console.log(Tokyo);
+// console.log(Dubai);
+// console.log(Paris);
+// console.log(Lima);
 
 function handleSubmit(event) {
   event.preventDefault();
